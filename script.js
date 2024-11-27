f241d3badf54238a676a9b6ff4a8940929d86693
princeshok <138206519+princeshok@users.noreply.github.com>
2024-11-27 16:39:12 +0000

    Add files via upload
    InnoHub: An innovation-driven platform designed to inspire, create, and connect ideas. InnoHub serves as a central space for showcasing groundbreaking solutions, fostering collaboration, and driving forward-thinking initiatives. Focused on empowering innovation, it bridges the gap between creativity and practical implementation.

---
+++ script.js
@@ -0,0 +1,5 @@
+
+    document.getElementById('hamburgerMenu').addEventListener('click', function() {
+        document.querySelector('.hamburger-container').classList.toggle('open');
+        document.getElementById('leftNavLinks').classList.toggle('open');
+    });
